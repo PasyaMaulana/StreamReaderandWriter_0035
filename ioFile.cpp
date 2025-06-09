b1582fbc4d950074ef1a42fb43633f38f80110c2
PasyaMaulana <pasyamaulana17@gmail.com>
2025-06-09 10:14:14 +0700

    jika file ada maka melakukan perulangan setiap baris

--- ioFile.cpp
+++ ioFile.cpp
@@ -36,4 +36,14 @@ int main()
 
     cout << endl
          << ">= Membuka dan membaca file " << endl;
+    // jika file ada maka
+    if (infile.is_open())
+    {
+        // melakukan perulangan setiap baris
+        while (getline(infile, baris))
+        {
+            // Dan tampilkan di sini
+            cout << baris << '\n';
+        }
+    }
 }
