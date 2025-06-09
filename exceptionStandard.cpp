d5722cde6fae463d992ad1c57d5c5d0692babb20
PasyaMaulana <pasyamaulana17@gmail.com>
2025-06-09 10:45:29 +0700

    hasil yang akan dieksesuki karena array data hanya memiliki 3 elemen

--- exceptionStandard.cpp
+++ exceptionStandard.cpp
@@ -18,5 +18,7 @@ int main()
     catch (exception &e)
     {
         // penangkap menggunakan obyek exception
+        cout << e.what() << endl;
+        /*akan dieksekusi karena array data hanya memiliki 3 elemen*/
     }
 }
