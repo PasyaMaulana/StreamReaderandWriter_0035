c8141f6448556c8fdc86bb493387799d9ce446ee
PasyaMaulana <pasyamaulana17@gmail.com>
2025-06-09 10:00:49 +0700

    menulise file atau keluar

--- ioFile.cpp
+++ ioFile.cpp
@@ -11,4 +11,6 @@ int main()
     ofstream outfile;
     // menunjuk ke sebuah nama file untuk membuka
     outfile.open("contohfile.txt");
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
 }
